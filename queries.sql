3917fa1a224d1f183a6f6cc34ff22f509b0d9acf
Danilo <dandam3006@gmail.com>
2022-07-28 17:59:18 -0300

    Question 3

--- queries.sql
+++ queries.sql
@@ -7,3 +7,10 @@ QUESTÃO 2:
 SELECT "userId" AS id, COUNT("endDate") AS educations FROM educations
 GROUP BY "userId";
 
+QUESTÃO 3:
+
+SELECT users.name AS writer, COUNT("writerId") AS testimonailCount FROM testimonials
+JOIN users ON users.id = "writerId"
+WHERE users.id = 435
+GROUP BY users.name ,"writerId";
+
