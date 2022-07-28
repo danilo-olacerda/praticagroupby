66c532c8d0d07c290db274bc857b03503f6ae802
Danilo <dandam3006@gmail.com>
2022-07-28 17:51:38 -0300

    Question 2

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 QUESTÃO 1:
 
-SELECT COUNT("endDate") as "currentExperiences" FROM experiences;
+SELECT COUNT("endDate") AS "currentExperiences" FROM experiences;
 
 QUESTÃO 2:
 
+SELECT "userId" AS id, COUNT("endDate") AS educations FROM educations
+GROUP BY "userId";
 
